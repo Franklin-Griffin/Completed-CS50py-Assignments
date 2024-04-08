e157a47dd6a6386710822d3368f237b37e26d626
Franklin Griffin <frankiegriffin107@gmail.com>
2024-04-08 17:37:47 -0400

    Update meal.py

--- PSet2/meal.py
+++ PSet2/meal.py
@@ -1,13 +1,14 @@
 def main():
     print("What time is it? ", end="")
-    convert(input())
-def convert(time):
-    h,m = map(int, time().split(":"))
-    if h == 7 or (h == 8 and m == 0):
+    time = convert(input())
+    if time >= 7 and time <= 8:
         print("breakfast time")
-    if h == 12 or (h == 13 and m == 0):
+    elif time >= 12 and time <= 13:
         print("lunch time")
-    if h == 18 or (h == 19 and m == 0):
+    elif time >= 18 and time <= 19:
         print("dinner time")
+def convert(time):
+    h,m = map(int, time.split(":"))
+    return h + m/60
 if __name__ == "__main__":
     main()

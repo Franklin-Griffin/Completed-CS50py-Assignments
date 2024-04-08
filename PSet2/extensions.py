9db76e245868811051f351dfd80090a1b9c30cfe
Franklin Griffin <frankiegriffin107@gmail.com>
2024-04-08 17:24:29 -0400

    Create extensions.py

---
+++ PSet2/extensions.py
@@ -0,0 +1,16 @@
+print("File name: ", end="")
+x = input().split(".")[-1]
+if x == "gif":
+	print("image/gif")
+elif x == "jpg" or x == "jpeg:
+	print("image/jpeg")
+elif x == "png":
+	print("image/png")
+elif x == "pdf":
+	print("application/pdf")
+elif x == "txt":
+	print("text/plain")
+elif x == "zip":
+	print("application/zip")
+else:
+	print("application/octet-stream")

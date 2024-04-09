d6d24c339b3f4931012d10bd7bb3f7650760d262
Franklin Griffin <frankiegriffin107@gmail.com>
2024-04-09 18:18:41 -0400

    Create fuel.py

---
+++ PSet3/fuel.py
@@ -0,0 +1,17 @@
+while True:
+	try:
+		print("Fraction: ", end="")
+		x,y = input.split("/")
+		if x > y:
+			continue
+		frac = int(x) / int(y)
+		if frac <= 0.01:
+			print("E")
+		elif frac >= 0.99:
+			print("F")
+		else:
+			print(f"{frac}%")
+	except (ValueError, ZeroDivisionError):
+		continue
+	else:
+		break

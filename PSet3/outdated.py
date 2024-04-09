b63a71ab1c344c8b83649986d7b36a597e5b3c0f
Franklin Griffin <frankiegriffin107@gmail.com>
2024-04-09 18:40:04 -0400

    Update outdated.py

--- PSet3/outdated.py
+++ PSet3/outdated.py
@@ -20,13 +20,13 @@ while True:
             x = date.split("/")
             if len(x) != 3:
                 continue
-            y,m,d = str(int(date[2])), str(int(date[0])), str(int(date[1]))
+            y,m,d = str(int(x[2])), str(int(x[0])), str(int(x[1]))
             print(f"{y.zfill(4)}-{m.zfill(2)}-{d.zfill(2)}")
         else:
             x = date.split(" ")
             if len(x) != 3:
                 continue
-            y,m,d = date[2], str(months.index(date[0]) + 1), date[1][:-1]
+            y,m,d = x[2], str(months.index(x[0]) + 1), x[1][:-1]
             print(f"{y.zfill(4)}-{m.zfill(2)}-{d.zfill(2)}")
         break
     except Exception:

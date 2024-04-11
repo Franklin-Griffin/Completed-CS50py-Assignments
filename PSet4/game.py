04b0d9866f791a09a6de5d908e8aef86014cef93
Franklin Griffin <frankiegriffin107@gmail.com>
2024-04-11 16:08:01 -0400

    Create game.py

---
+++ PSet4/game.py
@@ -0,0 +1,20 @@
+from random import random
+level = -1
+while level < 0:
+	try:
+		level = int(input("Level: "))
+	except Exception:
+		continue
+myNum = random() * level + 1
+while True:
+	try:
+		guess = int(input("Guess: "))
+		if guess == myNum:
+			print("Just right!")
+			break
+		if guess > myNum:
+			print("Too large!")
+		if guess < myNum:
+			print("Too small!")
+	except Exception:
+		continue

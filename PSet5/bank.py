b8836b4f5bc35292f5cc06ce194a6d0810099425
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 12:38:51 -0400

    Update bank.py

--- PSet5/bank.py
+++ PSet5/bank.py
@@ -4,10 +4,10 @@ def main():
     print(value(x))
 def value(greeting):
     if "Hello" in greeting:
-        return "$0"
+        return 0
     elif greeting[0] == "H":
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 if __name__ == "__main__":
     main()

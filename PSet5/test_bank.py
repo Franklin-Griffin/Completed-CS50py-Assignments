17494e4fde882ee6e5abccf1ec8500405f77d7ba
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 12:38:57 -0400

    Update test_bank.py

--- PSet5/test_bank.py
+++ PSet5/test_bank.py
@@ -1,7 +1,7 @@
 from bank import value
 def test_hello():
-	assert value("Hello") == "$0"
+	assert value("Hello") == 0
 def test_hi():
-	assert value("Hi") == "$20"
+	assert value("Hi") == 20
 def test_greetings():
-	assert value("Greetings") == "$100"
+	assert value("Greetings") == 100

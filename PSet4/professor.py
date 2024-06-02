ae590ddb1a545d16ec6f8188b6feb7bdedeced40
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 18:12:23 -0400

    Update professor.py

--- PSet4/professor.py
+++ PSet4/professor.py
@@ -1,26 +1,41 @@
-from random import random
-n = 0
-while not n in ["1", "2", "3"]:
-	n = input("Level: ")
-n = int(n)
-correct = 0
-questions = 0
-while questions < 10:
-	tries = 0
-	a = int(random() * (10 ** n)) + 1
-	b = int(random() * (10 ** n)) + 1
-	ans = a + b
-	resp = ""
-	while True:
-		if tries == 3:
-			print(f"{a} + {b} = {ans}")
-			break
-		resp = input(f"{a} + {b} = ")
-		if resp == str(ans):
-			correct += 1
-			break
-		else:
-			print("EEE")
-			tries += 1
-	questions += 1
-print(f"Score: {correct}")
+import random
+
+
+def main():
+	correct = 0
+	questions = 0
+	level = get_level()
+	while questions < 10:
+		tries = 0
+		a = generate_integer(level)
+		b = generate_integer(level)
+		ans = a + b
+		resp = ""
+		while True:
+			if tries == 3:
+				print(f"{a} + {b} = {ans}")
+				break
+			resp = input(f"{a} + {b} = ")
+			if resp == str(ans):
+				correct += 1
+				break
+			else:
+				print("EEE")
+				tries += 1
+		questions += 1
+	print(f"Score: {correct}")
+
+
+def get_level():
+	n = 0
+	while not n in ["1", "2", "3"]:
+		n = input("Level: ")
+	n = int(n)
+
+
+def generate_integer(level):
+	return int(random() * (10 ** level)) + 1
+
+
+if __name__ == "__main__":
+	main()

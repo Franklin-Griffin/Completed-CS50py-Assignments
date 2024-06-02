2aaadf8f0d77932bd7b012fc024d126aea676c3d
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 12:50:12 -0400

    Update test_fuel.py

--- PSet5/test_fuel.py
+++ PSet5/test_fuel.py
@@ -3,5 +3,7 @@ def test_convert():
 	assert convert("49/100") == 49
 def test_gauge():
 	assert gauge(0) == "E"
+	assert gauge(1) == "E"
+	assert gauge(99) == "F"
 	assert gauge(100) == "F"
 	assert gauge(49) == "49%"

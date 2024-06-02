ecd802aa033cd38886a3d275ea944d048d7f1f59
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 12:35:31 -0400

    Create bank.py

---
+++ PSet5/bank.py
@@ -0,0 +1,13 @@
+def main():
+    print("Greeting: ", end="")
+    x = input()
+    print(value(x))
+def value(greeting):
+    if "Hello" in greeting:
+        return "$0"
+    elif greeting[0] == "H":
+        return "$20"
+    else:
+        return "$100"
+if __name__ == "__main__":
+    main()

7abd6ae44beb2e532852558667b3fd7985764636
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-02 12:40:54 -0400

    Create plates.py

---
+++ PSet5/plates.py
@@ -0,0 +1,24 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+    startNums = False
+    for i in range(len(s)):
+        if not s[i].isalpha() and not s[i].isnumeric():
+            return False
+        if i <= 1 and not s[i].isalpha():
+            return False
+        if s[i].isalpha() and startNums:
+            return False
+        if s[i] == "0" and not startNums:
+            return False
+        if s[i].isnumeric():
+            startNums = True
+    return True
+if __name__ == "__main__":
+    main()

5ce4130bb2a4fbcd1a2616699685876ea681f1bd
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-17 13:39:07 -0400

    Create lines.py

---
+++ PSet6/lines.py
@@ -0,0 +1,17 @@
+import sys
+if len(sys.argv) < 2:
+	sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+	sys.exit("Too many command-line arguments")
+elif sys.argv[1].split(".")[-1] != "py":
+	sys.exit("Not a python file")
+else:
+	try:
+		with open(sys.argv[1], "r") as f:
+			count = 0
+			for line in f.readlines():
+				if not (line.strip() == "" or line.strip().startswith("#")):
+					count += 1
+			sys.exit(count)
+	except:
+		sys.exit("File does not exist")

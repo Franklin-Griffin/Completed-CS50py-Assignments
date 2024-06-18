660af197f9462d1c73a66239539d48cc95b3748f
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-18 15:05:04 -0400

    Update lines.py

--- PSet6/lines.py
+++ PSet6/lines.py
@@ -1,4 +1,5 @@
 import sys
+count = 0
 if len(sys.argv) < 2:
 	sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
@@ -12,6 +13,7 @@ else:
 			for line in f.readlines():
 				if not (line.strip() == "" or line.strip().startswith("#")):
 					count += 1
-			sys.exit(str(count))
 	except:
 		sys.exit("File does not exist")
+
+sys.exit(str(count))

5f979872c5274c944acd0d891b2fed0d23377f8a
Franklin Griffin <frankiegriffin107@gmail.com>
2024-06-18 12:21:01 -0400

    Update scourgify.py

--- PSet6/scourgify.py
+++ PSet6/scourgify.py
@@ -12,11 +12,11 @@ else:
 		with open(sys.argv[1], "r") as r:
 			with open(sys.argv[2], "w") as w:
 				x = r.readlines()
-				w.write("first,last,house")
+				w.write("first,last,house\n")
 				for row in x[1:]:
 					q = row.split(",")
 					q[0] = q[0][1:]
 					q[1] = q[1][:-1]
-					w.write(q[1]+","+q[0]+","+q[2])
+					w.write(q[1][1:]+","+q[0]+","+q[2])
 	except:
 		sys.exit("Could not read a file")
